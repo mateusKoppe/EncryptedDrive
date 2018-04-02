859a491cd6ece99dede7a2d0da52dc0351e252f9
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-01 22:32:36 -0300

    Add port option

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let app = require('./config/express')();
 let http = require('http').Server(app);
 
-http.listen(3030, () => console.log('Server is running'))
+const HTTP_PORT = process.env.HTTP_PORT || 3030;
+
+http.listen(HTTP_PORT, () => console.log('Server is running on port ' + HTTP_PORT));
